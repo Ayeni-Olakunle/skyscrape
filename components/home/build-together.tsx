dc01649e60fa0a54f5f8b0b6e9388fd9945c3512
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-02-09 14:23:17 +0100

    update 14

--- components/home/build-together.tsx
+++ components/home/build-together.tsx
@@ -39,7 +39,7 @@ export default function BuildTogether() {
           </div>
 
           {/* Quote Form */}
-          <div className="hidden lg:block bg-white rounded-xl shadow-xl p-6 mt-64 min-w-96">
+          <div className="hidden lg:block bg-white rounded-xl shadow-xl p-6 mt-64 w-[30rem]">
             <h3 className="text-lg font-semibold mb-4">Get a free Quote</h3>
 
             <form className="space-y-3">
@@ -53,11 +53,11 @@ export default function BuildTogether() {
                 <input
                   id="fullName"
                   placeholder="Your Name"
-                  className="w-full border rounded-lg px-4 py-2 text-sm outline-none focus:ring-2 mt-1"
+                  className="w-full border rounded-lg px-4 py-3 text-sm outline-none focus:ring-2 focus:ring-gray-300 mt-1 border-amber-500"
                 />
               </div>
 
-              <div>
+              <div className="mb-6">
                 <label
                   htmlFor="email"
                   className="block text-sm font-medium text-gray-700 mb-1"
@@ -67,11 +67,11 @@ export default function BuildTogether() {
                 <input
                   id="email"
                   placeholder="Your Email Address"
-                  className="w-full border rounded-lg px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-yellow-400 mt-1"
+                  className="w-full border rounded-lg px-4 py-3 text-sm outline-none focus:ring-2 focus:ring-gray-300 mt-1"
                 />
               </div>
 
-              <div>
+              <div className="mb-6">
                 <label
                   htmlFor="phone"
                   className="block text-sm font-medium text-gray-700 mb-1"
@@ -81,11 +81,11 @@ export default function BuildTogether() {
                 <input
                   id="phone"
                   placeholder="Your Phone Number"
-                  className="w-full border rounded-lg px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-yellow-400 mt-1"
+                  className="w-full border rounded-lg px-4 py-3 text-sm outline-none focus:ring-2 focus:ring-gray-300 mt-1"
                 />
               </div>
 
-              <div>
+              <div className="mb-6">
                 <label
                   htmlFor="Message"
                   className="block text-sm font-medium text-gray-700 mb-1"
@@ -95,8 +95,8 @@ export default function BuildTogether() {
                 <textarea
                   id="Message"
                   placeholder="Your Message"
-                  rows={3}
-                  className="w-full border rounded-lg px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-yellow-400 mt-1"
+                  rows={4}
+                  className="w-full border rounded-lg px-4 py-3 text-sm outline-none focus:ring-2 focus:ring-gray-300 mt-1"
                 />
               </div>
 
